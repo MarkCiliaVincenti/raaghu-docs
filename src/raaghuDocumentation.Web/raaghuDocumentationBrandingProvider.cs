2144180372b7fc99f664b42af1f0566162836448
suraj-khupse <99809670+suraj-khupse@users.noreply.github.com>
2023-11-24 18:38:42 +0530

    favicon and set title for raaghu docs.

--- src/raaghuDocumentation.Web/raaghuDocumentationBrandingProvider.cs
+++ src/raaghuDocumentation.Web/raaghuDocumentationBrandingProvider.cs
@@ -1,10 +1,12 @@
 ﻿using Volo.Abp.Ui.Branding;
 using Volo.Abp.DependencyInjection;
+using Volo.Abp.AspNetCore.Mvc.UI.Layout;
+using raaghuDocumentation.Web.Menus;
 
 namespace raaghuDocumentation.Web;
 
 [Dependency(ReplaceServices = true)]
 public class raaghuDocumentationBrandingProvider : DefaultBrandingProvider
 {
-    public override string AppName => "raaghuDocumentation";
+    public override string AppName => "Documention Center | Raaghu.io";
 }
